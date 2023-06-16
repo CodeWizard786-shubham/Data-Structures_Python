70ae86ce160460bf0bf360957d92138f38a48e3e
shubham shirke <shirkeshubham84@gmail.com>
2023-06-17 00:17:07 +0530

    [shubham] Add:Python program to remove an item if it is present in the set.

---
+++ set/05_remove_from_set.py
@@ -0,0 +1,39 @@
+'''
+@Author: shubham shirke
+@Date: 2023-06-10 10:35:30
+@Last Modified by: shubham shirke
+@Last Modified time: 2023-06-10 10:35:30
+@Title : Python program to remove a members in the set if it is present.
+'''
+
+def get_elements_in_set(set_of_elements,element):
+    """
+    Descripition : 
+            This function removes an element given as a input by user if it is present in the set or returns false.
+    Paremeters  :
+            set_of_elements : contains set of elements.
+            element : value to add in the set.
+    Retruns     :
+            set_of_elements or False.
+    """
+    if element in set_of_elements:
+        set_of_elements.remove(element)
+    else:
+        return False
+
+
+    return set_of_elements
+
+
+# main
+def main():
+    set_of_elements = set(input("Enter elements separated by spaces: ").split())
+    element_to_add = eval(input("Enter element to add in the set: "))
+    result = get_elements_in_set(set_of_elements,element_to_add)
+    print(f"The result set is: {result}")
+
+
+if __name__ == "__main__":
+    main()
+
+
